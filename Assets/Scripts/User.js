350c25466f4c6d2d1f358e1ce2c5b0ff67c466a5
Asep Maulana Ismail <asepmaulanaismail@gmail.com>
2014-11-29 22:38:14 +0700

    Update cruds for users

---
+++ Assets/Scripts/User.js
@@ -0,0 +1,161 @@
+// User
+var User = Backbone.Model.extend({
+    initialize: function(user) {
+		//debugger;;
+		if (user != undefined){
+			this.username = user.username;
+			this.password = user.password;
+			this.first_name = user.first_name;
+			this.last_name = user.last_name;
+			this.email = user.email;
+		}
+	}
+});
+
+var Users = Backbone.Collection.extend({
+	model: User,
+	url: apiUrl + 'user/',
+	parse: function(response){
+		return response.objects;
+	},
+	// Overwrite the sync method to pass over the Same Origin Policy
+	sync: function(method, model, options) {
+		var that = this;
+			var params = _.extend({
+				type: 'GET',
+				dataType: 'jsonp',
+				url: that.url,
+				processData: false
+			}, options);
+
+		return $.ajax(params);
+	},
+	setParams: function(params){
+		this.url = apiUrl + 'user/';
+		this.url += "?" + params;
+	}
+});
+
+var UserView = Backbone.View.extend({
+    initialize: function(users) {
+		////debugger;;
+		_.bindAll(this, 'render', 'renderHeader');
+
+		// create a collection
+
+		if (users != undefined){
+			this.collection = users;
+		}else{
+			this.collection = new Users();
+		}
+    },
+	
+    // Use an extern template
+    //template: _.template($("#userTemplate")),
+
+    render: function() {
+		removeRenderedPart();
+		
+        // Fill the html with the template and the collection
+		// Fetch the collection and call render() method
+		var that = this;
+		//that.renderSearch();
+		that.renderHeader(["Username","Name", "Email", "Role", "Action"]);
+
+		//fetch all data
+		this.collection.fetch({
+			success: function(){
+				console.log("fetch all data success.");
+		
+				// redeclare
+				that.el = $(idTbl);
+				
+				var rowNum = 1;
+				
+				_.each(that.collection.toJSON(), function(user){
+					var html = "";
+					html += ("<tr>");
+					html += ("<td>");
+					if (user.username != "admin"){
+						html += (checkBox);
+					}
+					html += (id.replace('{0}', user.id));
+					html += ("</td>");
+					html += ("<td>");
+					html += (rowNum); 
+					html += ("</td>");
+					html += ("<td>");
+					html += (user.username); 
+					html += ("</td>");
+					html += ("<td>");
+					html += (user.first_name + " " + user.last_name);
+					html += ("</td>");
+					html += ("<td>");
+					html += (user.email); 
+					html += ("</td>");
+					html += ("<td>");
+					html += (user.is_superuser == true)? "Admin" : "Staff"; 
+					html += ("</td>");
+					html += ("<td>");
+					
+					if (user.username != "admin"){
+						html += ("<input class='btn btn-success' type='button' value='" + ((user.is_superuser != true)? "Make as admin" : "Cancel admin") + "' onClick='toogleAdmin(this, " + user.id + ", "+ ((user.is_superuser != true)? "true" : "false") +")' />"); 
+					}
+					html += ("</td>");
+					html += ("</tr>");
+					
+					$(that.el).append(html);
+					
+					rowNum++;
+				});
+			}
+		});
+    },
+
+    renderHeader: function(headers) {
+        // Fill the html with the template and the collection
+		var html = "";
+		html += ("<table class='table table-hover' id='" + tblId + "'>");
+		html += ("<tr>");
+		html += ("<th>");
+		html += ("#"); // checkboxes
+		html += ("</th>");
+		html += ("<th>");
+		html += ("No."); // no.
+		html += ("</th>");
+		
+		_.each(headers, function(text){
+			html += ("<th>");
+			html += (text);
+			html += ("</th>");
+		});
+		
+		html += ("</tr>");
+		html += ("</table>");
+		$('#renderArea').append(html);
+			
+    },
+	
+	renderSearch: function(){
+		var html = "";
+		html += ("<table class='table table-hover' id='" + tblId + "'>");
+		html += ("<tr>");
+		html += ("<th>");
+		html += ("#"); // checkboxes
+		html += ("</th>");
+		html += ("<th>");
+		html += ("No."); // no.
+		html += ("</th>");
+		
+		_.each(headers, function(text){
+			html += ("<th>");
+			html += (text);
+			html += ("</th>");
+		});
+		
+		html += ("</tr>");
+		html += ("</table>");
+		$('#serchArea').append(html);
+		
+	}
+});
--- Assets/Scripts/common.js
+++ Assets/Scripts/common.js
@@ -20,21 +20,72 @@ function removeRenderedPart(){
 }
 
 function removeActivedMenu(){
-	$("li").removeClass("active")
+	$("li").removeClass("active");
 }
 
-function users(li){
-	removeRenderedPart();
-	removeActivedMenu();
-	$('#title').text("Users");
-	var btnRender = "";
-	btnRender += '<button class="btn btn-primary" id="btnAdd" type="button">Add</button>';
-	btnRender += '<button class="btn btn-primary disabled" id="btnEdit" type="button">Edit</button>';
-	btnRender += '<button class="btn btn-primary disabled" id="btnDelete" type="button">Delete</button>';
-						
-	$('#btnArea').html(btnRender);
-						
-	var app = new UserView();
+function goToFile(cat){
+	window.location = cat + ".html";
+}
+
+function loading(e){
+	var loading = "";
+	loading += '<div id="loader" style="display:block; background-color: #fff;left: 0; top: 0; position:absolute; width:100%;  text-align:center;">';
+	loading += '	<img id="loaderImg" src="Assets/Images/loader.gif" title="Loading..."/>';
+	loading += '</div>';
 	
-	$(li).addClass("active");
+	if (e == "show"){
+		$("body").append(loading);
+	}else if (e == "hide"){
+		$("#loader").hide({effect: "fade", duration: 300});
+		$("#loader").remove();
+	}
+}
+
+function showMessageAlert(msg){
+	var div = "";
+	div += '<div class="alert alert-error">';
+	div += '  <button type="button" class="close" data-dismiss="alert">&times;</button>';
+	div += '  <strong>Error!</strong> ' + msg;
+	div += '</div>';
+	
+	$("#msgArea").html("");
+	
+	$("#msgArea").append(div);
+}
+
+function writeCookie(name,value,days) {
+    var date, expires;
+    if (days) {
+        date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        expires = "; expires=" + date.toGMTString();
+            }else{
+        expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function readCookie(name) {
+    var i, c, ca, nameEQ = name + "=";
+    ca = document.cookie.split(';');
+    for(i=0;i < ca.length;i++) {
+        c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1,c.length);
+        }
+        if (c.indexOf(nameEQ) == 0) {
+            return c.substring(nameEQ.length,c.length);
+        }
+    }
+    return '';
+}
+
+function doLogout(){
+	writeCookie("sessionId", "", 1);
+	writeCookie("username", "", 1);
+	writeCookie("password", "", 1);
+	writeCookie("isLoggedIn", "", 1);
+	writeCookie("Authorization", "", 1);
+	
+	window.location = "index.html";
 }
